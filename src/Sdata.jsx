1395630e8efcf0cf6d5920a1673292a09390dd7c
Akshat <akshatsoni1997.pmck@gmail.com>
2022-04-11 14:26:52 +0530

    my first commit reactwebsite

---
+++ src/Sdata.jsx
@@ -0,0 +1,44 @@
+import android from "../src/Images/android.png";
+import digital from "../src/Images/digital.jpg";
+import marketing from "../src/Images/marketing.jpeg";
+import web from "../src/Images/web.jpeg";
+import software from "../src/Images/software.jpeg";
+import app from "../src/Images/app.jpeg";
+
+
+
+
+const Sdata =[
+    {
+        imgsrc: web,
+        title: "web development",
+
+    },
+    {
+        imgsrc: app,
+        title: "app development",
+
+    },
+    {
+        imgsrc: software,
+        title: "software development",
+
+    },
+    {
+        imgsrc: digital,
+        title: "Digital Marketing",
+
+    },
+    {
+        imgsrc: android,
+        title: "android development",
+
+    },
+    {
+        imgsrc: marketing,
+        title: "marketing development",
+
+    }
+]
+
+export default Sdata;
